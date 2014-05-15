503c3084f618563791c36db67a2103df8504653d
wadadaaa <lopatinski@gmail.com>
2014-05-15 04:16:23 +0300

    config for deploy

--- chilliboom/settings.py
+++ chilliboom/settings.py
@@ -13,7 +13,7 @@ RAVEN_CONFIG = {
 SECRET_KEY = env('SECRET_KEY', '#b7&!k2cxgw5+s$%s&p#+!_8=*lo9mv-3*p0gsozvs3%myb(=k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TEMPLATE_DEBUG = env('DEV', False)
+DEBUG = TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
