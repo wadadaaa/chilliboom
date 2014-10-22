af6379ea9bc1c278bf74414b8cadf631ffe80313
wadadaaa <lopatinski@gmail.com>
2014-10-22 05:38:39 +0300

    update allow hosts

--- chilliboom/settings.py
+++ chilliboom/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
